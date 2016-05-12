2e2128805caaaab2b428b243ed77220873aa8c13
Anthony Caiafa <acaiafa1@bloomberg.net>
2016-05-12 13:52:46 -0400

    updating metadata

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,13 @@
 name 'grub'
 maintainer 'Bloomberg L.P.'
-maintainer_email 'you@example.com'
+maintainer_email 'acaiafa1@bloomberg.net'
 license 'all_rights'
-description 'Installs/Configures grub'
-long_description 'Installs/Configures grub'
+description 'Configures grub'
+long_description 'Configures grub'
 version '0.1.0'
 
 supports 'centos', '>= 6.4'
 supports 'redhat', '>= 6.4'
 supports 'ubuntu', '>= 12.04'
-supports 'aix'
-supports 'solaris2'
-
 
 depends 'poise'
