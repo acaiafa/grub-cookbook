c5d8cde15d40be402685bd35a8c769151f95aa98
Richard Mcleod <rmcleod8@ip-1-71-67-19.drr1.bcpc.bloomberg.com>
2018-11-28 16:11:32 -0500

    bumps version

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,8 @@ maintainer 'Bloomberg L.P.'
 maintainer_email 'acaiafa1@bloomberg.net'
 description 'Configures grub'
 long_description 'Configures grub'
-version '0.1.2'
+version '0.2.0'
 
 supports 'centos', '>= 6.4'
 supports 'redhat', '>= 6.4'
 supports 'ubuntu', '>= 12.04'
-
-depends 'poise'
